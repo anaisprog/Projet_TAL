b5218b018d68da58817b8de686144e1c83cbff95
Anais Mokdad <amokdad@a-018818.ups.u-psud.fr>
2018-03-30 14:39:18 +0200

    mode2

---
+++ src/chatbot_niv2.py
@@ -0,0 +1,31 @@
+import random
+
+
+# define the function blocks
+def reponse1():
+    print ("uh huh.\n")
+
+def reponse2():
+    print ("Hum....\n")
+
+def reponse3():
+    print ("Continuez\n")
+
+def reponse4():
+    print ("Oui...\n")
+    
+while(1):	
+	chatQuestion = random... /#TODO
+	rep = raw_input(chatQuestion)
+	
+	nombre = random.randint(0,3)
+	if(nombre == 0):
+		reponse1()
+	
+	if (nombre == 1):
+		reponse2()
+	
+	else:
+		reponse3()
+
+
